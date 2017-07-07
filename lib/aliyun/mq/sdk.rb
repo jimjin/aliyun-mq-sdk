13003c83f04b1efe7012452c698b0b87c6e87161
Jim <jim.jin2006@gmail.com>
2017-07-07 16:56:13 +0800

    init.

---
+++ lib/aliyun/mq/sdk.rb
@@ -0,0 +1,9 @@
+require "aliyun/mq/sdk/version"
+
+module Aliyun
+  module Mq
+    module Sdk
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/aliyun/mq/sdk/version.rb
@@ -0,0 +1,7 @@
+module Aliyun
+  module Mq
+    module Sdk
+      VERSION = "0.1.0"
+    end
+  end
+end
